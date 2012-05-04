6f42f796062be545285bd787c79ef27b7dc6cd93
Cusco Tsang <cttsang22@gmail.com>
2012-05-05 06:28:28 +0800

    initial commit template

---
+++ application/config/foreign_chars_chinese.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Academic Free License version 3.0
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0) that is
+ * bundled with this package in the files license_afl.txt / license_afl.rst.
+ * It is also available through the world wide web at this URL:
+ * http://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * licensing@ellislab.com so we can send you a copy immediately.
+ *
+ * @package		CodeIgniter
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2012, EllisLab, Inc. (http://ellislab.com/)
+ * @license		http://opensource.org/licenses/AFL-3.0 Academic Free License (AFL 3.0)
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+/**
+ * CodeIgniter Chinese Convert
+ *
+ * @package		CodeIgniter
+ * @subpackage	Library
+ * @category	Library
+ * @author		Cusco Tsang
+ */
+
+/*
+| -------------------------------------------------------------------
+| Chinese Convert Characters
+| -------------------------------------------------------------------
+| Below character set contains an array of chinese characters for
+| conversion used by the UTF-8 Chinese Convert
+|
+*/
+
+/*
+| -------------------------------------------------------------------
+| Single to Single zh-CHT to zh-CHS Table
+| -------------------------------------------------------------------
+| Below character set contains an array of chinese characters for
+| conversion used by the UTF-8 Chinese Convert General
+|
+*/
+
+
+
+/* End of file foreign_chars_chinese.php */
+/* Location: ./application/config/foreign_chars_chinese.php */
---
+++ application/libraries/Convert_han.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Convert Han Class
+ *
+ * Convert Han Base Class
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		Cusco Tsang
+ */
+
+class Convert_han
+{
+	
+	public function __construct()
+	{
+
+	}
+
+}
+
+/* End of file Convert_han.php */
+/* Location: ./application/libraries/Convert_han.php */
